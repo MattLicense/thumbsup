d507f9e12c0ac2491ae64d901a99488bf124d3f5
Matt License <matt@mattlicense.co.uk>
2013-05-12 09:34:09 +0100

    Removed links to Laravel framework. Added attributes option for <img> tag

--- README.md
+++ README.md
@@ -1,39 +1,24 @@
 ThumbsUp
 --------
 
-A Laravel bundle to crop jpeg, png, gif images to set dimensions.
-
-Install
--------
-
-Using the Artisan CLI, you can use the following command:
-
-<code>php artisan bundle:install thumbsup</code>
-
-Then you'll need to register the bundle in <code>application/bundles.php</code>
-
-<code>'thumbsup' => array('auto' => true)</code>
-
-And add the alias to <code>application/config/application.php</code> (you don't have to, but it looks nicer in your code)
-
-<code>'Thumbsup' => 'Thumbsup\\\Thumbsup',</code>
+A PHP script to crop jpeg, png, gif images to set dimensions.
 
 Usage
 -----
 
 Once you've got everything set up, you can create thumbnails from local images by using:
 
-<code>Thumbsup::crop('/path/to/img')</code>
+<code>MattLicense\Thumbsup::crop('/path/to/img')</code>
 
 By default, this will create a 250x250 thumbnail from the top left corner of the image. You can also create different sized thumbnails, from a different point on the image using:
 
-<code>Thumbsup::crop('path/to/img', 300, 150, 100, 50)</code>
+<code>MattLicense\Thumbsup::crop('/path/to/img', array('width' => 300, 'height' => 150), array('x' => 100, 'y' => 50))</code>
 
 This will create a 300x150 image starting 100px from the left and 50px from the top of the image.
 
-<b>Note:</b> The path you specify should be from your base domain. E.g. <code>/path/to/img</code> should be <code>http://www.yourdomain.com/path/to/img</code>
+You can also add HTML attributes to the image:
 
-All thumbnails will be saved in /img/thumbs by default (if this folder doesn't exist, it will be created).
+<code>MattLicense\Thumbsup::crop('/path/to/img', array('width' => 300, 'height' => 150), array('x' => 100, 'y' => 50), array('class' => 'img-class', 'alt' => 'cropped image'))</code>
 
 Licence
 -------
--- thumbsup.php
+++ thumbsup.php
@@ -1,66 +1,46 @@
 <?php
-namespace Thumbsup;
 /**
  * thumbsup.php
- * ThumbsUp. A Laravel bundle to crop images to a set dimension
- * @author  Matt License (matt@mattlicense.co.uk)
- * Date     January 2013
+ * A small PHP script for cropping gif, jpeg and png images
+ * @author Matt License (http://www.mattlicense.co.uk)
  */
 
-class Thumbsup {
+namespace MattLicense;
 
-    /**
-     * @var array       $fileTypes
-     * Allowed file types pointing to php imaging functions and file extensions
-     * @default png|jpg|gif
-     */
-    static $mimeTypes = array(
+class ThumbsUp
+{
+    static protected $maxSize = 1048576;
+
+    static protected $location = "/img/thumbs";
+
+    static protected $mimeTypes = array(
         'image/png'     => array('create' => 'imagecreatefrompng', 'generate' => 'imagepng', 'ext' => 'png'),
         'image/jpeg'    => array('create' => 'imagecreatefromjpeg', 'generate' => 'imagejpeg', 'ext' => 'jpeg'),
         'image/gif'     => array('create' => 'imagecreatefromgif', 'generate' => 'imagegif', 'ext' => 'gif'),
     );
 
-    /**
-     * @var integer     $maxSize
-     * Maximum file size in bytes
-     * @default 1Mb (1048576b)
-     */
-    static $maxSize = 1048576;
-
-    /**
-     * @var string      $saveFolder
-     * Folder in which the thumbnails will be saved
-     * @default 'thumbs'
-     */
-    static $saveFolder = 'thumbs';
-
-    /**
-     * crop()
-     * Crop a base image to a fixed size (@default 250x250), from fixed coordinates (@default [0,0] - top left corner)
-     * @param string    $imgSrc     location of the base image
-     * @param integer   $x          x coordinate to start the crop      @default 0
-     * @param integer   $y          y coordinate to start the crop      @default 0
-     * @param integer   $width      cropped width                       @default 250px
-     * @param integer   $height     cropped height                      @default 250px
-     * @return string|false
-     * @throws Exception
-     */
-    public static function crop($imgSrc, $width = 250, $height = 250, $x = 0, $y = 0) {
-        //
-        // make sure that the image points towards the public folder
-        $imgSrc = $_SERVER['DOCUMENT_ROOT'] . $imgSrc;
-
-        //
-        // check that the file exists
+    static public function crop($imgSrc,
+                                array $dimensions = array('width' => 250, 'height' => 250),
+                                array $crop = array('x' => 0, 'y' => 0),
+                                array $attributes = array())
+    {
+        // check the image parameters
         if(!file_exists($imgSrc))
-            throw new \Exception('File ' . $imgSrc . ' not found');
+            throw new IllegalArgumentException("Image {$imgSrc} not found.");
 
-        //
-        // check that the file is less than the maximum
         if(filesize($imgSrc) > static::$maxSize)
-            throw new \Exception('File is bigger than ' . static::$maxSize/1024 . 'Kb');
+            throw new IllegalArgumentException('File is bigger than ' . static::$maxSize/1024 . 'Kb');
 
-        //
+        if(!(array_key_exists('width', $dimensions) && array_key_exists('height', $dimensions))
+            throw new IllegalArgumentException("The dimensions array must have both height and width keys");
+
+        if(!(array_key_exists('x', $crop) && array_key_exists('y', $crop)))
+            throw new IllegalArgumentException("The crop array must have both x and y keys");
+
+        $x = $crop['x']; $y = $crop['y'];
+        $width = $dimensions['width'];
+        $height = $dimensions['height'];
+
         // get the base image dimensions and check that the thumbnail will be within dimensions
         list($srcWidth, $srcHeight) = getimagesize($imgSrc);
         if(($x + $width) > $srcWidth)
@@ -68,61 +48,50 @@ class Thumbsup {
         if(($y + $height) > $srcHeight)
             throw new \Exception('Thumbnail over-extends image height by ' . ($y + $height - $srcHeight) . ' pixels');
 
-        //
         // get the mime type
         $finfo = new \finfo();
         $fileType = $finfo->file($imgSrc, FILEINFO_MIME_TYPE);
 
-        //
         // check the file type is valid
         if(!array_key_exists($fileType, static::$mimeTypes))
-            throw new \Exception('Mime type '.$fileType.' not supported by Thumbsup bundle.');
+            throw new Exception("Mime type {$fileType} is not supported.");
 
-        //
         // get the function to create the thumbnail
         $createImageFrom    = static::$mimeTypes[$fileType]['create'];
         $imageFrom          = static::$mimeTypes[$fileType]['generate'];
         $mimeExtension      = static::$mimeTypes[$fileType]['ext'];
 
-        //
-        // save the image to /public/img/thumbs (by default)
-        $saveTo = 'img' . DS . static::$saveFolder;
-
-        //
         // generate a name
         $saveName = md5($imgSrc . $x . $y . $width . $height) . '.' . $mimeExtension;
 
-        //
         // if the save folder doesn't exist, create it with the
-        if(!is_dir(path('public') . $saveTo))
-            mkdir(path('public') . $saveTo);
+        if(!is_dir(realpath(static::$location))) mkdir(realpath(static::$location));
 
-        //
         // file location
-        $savePath = $saveTo . DS . $saveName;
+        $savePath = realpath(static::$location) . DIRECTORY_SEPARATOR . $saveName;
 
-        //
         // make sure we've haven't made this thumbnail before
         if(!file_exists($savePath)) {
-            //
             // create the image
             $src = $createImageFrom($imgSrc);
             $dest = imagecreatetruecolor($width, $height);
 
             imagecopy($dest, $src, 0, 0, $x, $y, $width, $height);
 
-            //
             // copy the image to the output buffer and destroy the image to free up resources
             ob_start();
             $imageFrom($dest);
             $output = ob_get_clean();
             imagedestroy($dest);
 
-            //
-            // save the buffer and return the img tag
-            \File::put(path('public') . $savePath, $output) ? $saveName : false;
+            // save the buffer to file
+            file_put_contents($savePath, $output);
         }
 
-        return '<img src="' . $savePath . '">';
+        // explode the attributes
+        $att = '';
+        foreach($attributes as $key => $val) $att .= $key .'="'.$val.'" ';
+
+        return '<img src="' . $savePath . '" '.trim($att).'>';
     }
 }
